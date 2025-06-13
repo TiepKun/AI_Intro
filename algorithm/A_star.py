0e93db02d8b99b13a7b14e8db0ef77862e30511a
HoanxHoan <146335893+HoanxHoan@users.noreply.github.com>
2025-06-13 17:47:55 +0700

    g

--- algorithm/A_star.py
+++ algorithm/A_star.py
@@ -3,7 +3,7 @@ import math
 # A* heuristic
 def heuristic(a, b):
     #return ((a[0]-b[0])**2 + (a[1]-b[1])**2) ** 0.5
-    return 1.5 * math.hypot(a[0] - b[0], a[1] - b[1])
+    return math.hypot(a[0] - b[0], a[1] - b[1])
 
 def astar(graph, start, goal, positions):
     queue = []
